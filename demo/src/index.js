1c6abb5b8c43875486dccec065e7a6da0a811bb0
Thien Bui <thienbui797@gmail.com>
2017-08-05 16:19:51 +1000

    update demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,20 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import Lmgtfy from '../../src';
+const App = () =>{
+    return (
+            <div>
+                <div>
+                    <Lmgtfy query="bing">Search "Bing" on "Google" in current Frame</Lmgtfy>
+                </div>
+                <div>
+                    <Lmgtfy query="google" engine="bing">Search "Google" on "Bing" in current Frame</Lmgtfy>
+                </div>
+                <div>
+                    <Lmgtfy query="duckduckgo" target="_blank"> Search "duckduckgo" on "Google" in new tab </Lmgtfy>
+                </div>
+            </div>
+            );
+}
 
-render(<Lmgtfy query="google" engine="bing">Search "Google" on "Bing"</Lmgtfy>, document.querySelector('#demo'))
+render(<App/>, document.querySelector('#demo'))
